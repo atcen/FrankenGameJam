b31ec0d0f80ba90378961d403af1391897694802
Domda7 <92846869+Domda7@users.noreply.github.com>
2022-11-06 11:36:04 +0100

    transformation on start

--- Assets/Scripts/Enemies/MinionLogic.cs
+++ Assets/Scripts/Enemies/MinionLogic.cs
@@ -7,7 +7,8 @@ public class MinionLogic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float scale = Random.Range(1f, 3f);
+        transform.localScale = new Vector2(scale, scale);
     }
 
     // Update is called once per frame
